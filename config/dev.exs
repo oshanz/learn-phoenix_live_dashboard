bec4785d159120bd9a473a690c16e400b1cf6b38
Oshan Wisumperuma <warunaoshan@gmail.com>
2021-11-09 14:03:26 +0530

    config docker db

--- config/dev.exs
+++ config/dev.exs
@@ -3,9 +3,10 @@ import Config
 # Configure your database
 config :app, App.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   database: "app_dev",
   hostname: "localhost",
+  port: "5433",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
